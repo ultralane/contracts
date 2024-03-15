72840514779a40fbb71e00f23c5b4ed9ba56bdee
Soham Zemse <22412996+zemse@users.noreply.github.com>
2024-03-15 22:09:54 +0000

    install openzeppelin and create token

---
+++ contracts/USDC.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract USDC is ERC20 {
+    constructor() ERC20("USDC", "USDC") {
+        _mint(msg.sender, 1_000_000_000e6);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+}
