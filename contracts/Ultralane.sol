29f7b77cb49dd6b13f77171bb64b39e7c4a26b71
Soham Zemse <22412996+zemse@users.noreply.github.com>
2024-03-17 00:59:05 +0000

    add function to perform tx

--- contracts/Ultralane.sol
+++ contracts/Ultralane.sol
@@ -1,11 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 import {MixerPool, SplitJoin16Verifier, Hash2Verifier, NoteVerifier, IERC20} from "./MixerPool.sol";
 import {TrustlessWithdraw, Input16Verifier, IMailbox} from "./TrustlessWithdraw.sol";
 
-contract Ultralane is MixerPool, TrustlessWithdraw {
+contract Ultralane is Ownable, MixerPool, TrustlessWithdraw {
     constructor(
+        address owner,
         IERC20 usdc,
         SplitJoin16Verifier splitJoinVerifier,
         Hash2Verifier hash2Verifier,
@@ -15,5 +18,6 @@ contract Ultralane is MixerPool, TrustlessWithdraw {
     )
         MixerPool(usdc, splitJoinVerifier, hash2Verifier, noteVerifier)
         TrustlessWithdraw(input16Verifier, mailbox)
+        Ownable(owner)
     {}
 }
